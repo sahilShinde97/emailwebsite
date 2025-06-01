56a4242daa30c650418395acd03b463aacb27f22
Your Name <your.email@example.com>
2025-06-01 23:59:21 +0530

    still facing same error

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import connectCloudinary from "./configs/cloudinary.js";
 import courseRouter from "./routes/courseRoute.js";
 import userRouter from "./routes/userRoutes.js";
 
+
 // initialize express
 const app = express();
 
@@ -16,22 +17,19 @@ const app = express();
 await connectDB();
 await connectCloudinary();
 
-// Basic middleware
+
+//middleware
 app.use(cors());
-app.use(clerkMiddleware());
-
-// Stripe webhook route with raw body parser
-app.post('/stripe-webhook', express.raw({ type: '*/*' }), stripeWebhooks);
-
-// For all other routes, use JSON parsing
-app.use(express.json());
+app.use(clerkMiddleware())
 
 //routes
 app.get("/", (req, res) => res.send("API working"));
-app.post('/clerk', clerkWebhooks);
-app.use('/api/educator', educatorRouter);
-app.use("/api/course", courseRouter);
-app.use('/api/user', userRouter);
+app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator',express.json(), educatorRouter)
+app.use("/api/course", express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({ type: 'application/json'}), stripeWebhooks)
+
 
 // port
 const PORT = process.env.PORT || 5000;
